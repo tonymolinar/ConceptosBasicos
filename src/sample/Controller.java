60b958ef00aa98da64361664dff89c2d314bd228
tonymolinar <71780333+tonymolinar@users.noreply.github.com>
2020-09-27 21:01:28 -0600

    ConceptosBasicos
    ConceptosBasicos

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,16 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML TextField txtedad;
+    @FXML Label labeltitulo;
+
+    public void evento(ActionEvent event){
+        int dias = Integer.parseInt(txtedad.getText())*365;
+        labeltitulo.setText("Has vivido "+dias+" dias");
+    }
 }
